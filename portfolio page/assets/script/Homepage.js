f2442c7a6e9eb2e25ccb16859c512854e9d36f02
Aiden Brennan <Aiden.c.b@hotmail.com>
2022-01-31 19:37:32 +1100

    add files via upload

---
+++ portfolio page/assets/script/Homepage.js
@@ -0,0 +1,11 @@
+$("#AboutMeBtn").on("click", function(){
+    location.href = "about me.html";
+})
+
+$("#MyProjectsBtn").on("click", function(){
+    location.href = "My Projects.html";
+})
+
+$("#HomeBtn").on("click", function(){
+    location.href = "index.html";
+})
